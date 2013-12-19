904512374ff6a65006dc47a252fa7147004185de
Harry Jeffery <harry@exec64.co.uk>
2013-12-19 14:43:45 +0000

    Implemented basic init/close functions

--- gameboy.c
+++ gameboy.c
@@ -1,17 +1,28 @@
 #include "gameboy.h"
 
 #include <stdint.h>
+#include <stdlib.h>
 #include <errno.h>
 
 
 int8_t gameboy_init(gameboy_t* gb)
 {
-    return -ENOSYS;
+    if(!gb) {
+        return -EINVAL;
+    }
+
+    gb->mem = calloc(0x10000, 0x1);
+    return 0;
 }
 
 int8_t gameboy_close(gameboy_t* gb)
 {
-    return -ENOSYS;
+    if(!gb) {
+        return -EINVAL;
+    }
+
+    free(gb->mem);
+    return 0;
 }
 
 int8_t gameboy_reset(gameboy_t* gb)
